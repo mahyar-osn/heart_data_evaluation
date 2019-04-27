5bae1027ff33f01e5c531b4dabd3f80a84ee72ac
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2019-04-27 16:12:31 +1200

    moved Reginalize class to regional_segmentation.py for better access.

--- analysis.py
+++ analysis.py
@@ -4,27 +4,10 @@ import pandas as pd
 import numpy as np
 
 import read_write_data as io
-import regional_segmentation as rs
 
 from src.analysis import CSV, Plotting, Stats
 
-
-class Regionalize:
-    def __init__(self, data_frame, region_indx):
-        self._df = data_frame
-        self._indx = region_indx
-        self._array_size = region_indx.shape[0]
-        self._array = np.zeros((self._array_size, data_frame.shape[1]))
-        self._header = list(self._df.columns.values)
-
-    def get_data(self):
-        counter = 0
-        for i in self._df['Region']:
-            if i in self._indx:
-                self._array[counter] = self._df[self._df['Region'] == i]
-                counter += 1
-
-        return pd.DataFrame(self._array, columns=self._header)
+from regional_segmentation import get_regions
 
 
 """ A dict for some parameter configuration """
@@ -64,10 +47,8 @@ region = np.linspace(1, 600, 600, dtype=np.int)
 df['Region'] = pd.Series(region, index=df.index)
 
 
-""" Regionalizing """
-oft_indx = rs.OFT()
-REG = Regionalize(df, oft_indx)
-oft = REG.get_data()
+""" Get heart regions """
+regions = get_regions(df)
 
 
 """ Plotiing """
@@ -80,6 +61,8 @@ output_file = os.path.join(output_path, scatter_img_file)
 plot.plotSimpleScatter(x, y, df, save=True, path=output_file)
 
 print('done')
+
+
 
 
 
